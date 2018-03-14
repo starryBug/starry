b4d8e34d263b036501ab2b1addd6246f0b278ddb
lizhitao <lizhitao@sky.com>
2018-03-15 00:25:28 +0800

    fdfs init

---
+++ starry-mobile/src/main/java/com/starry/util/FastDFSClientWrapper.java
@@ -0,0 +1,120 @@
+package com.starry.util;
+
+import com.github.tobato.fastdfs.conn.FdfsWebServer;
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.exception.FdfsUnsupportStorePathException;
+import com.github.tobato.fastdfs.proto.storage.DownloadCallback;
+import com.github.tobato.fastdfs.service.AppendFileStorageClient;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+/**
+ * Created by lzt11 on 2018/2/18.
+ */
+@Slf4j
+@AllArgsConstructor
+@Component
+@ConfigurationProperties(value = "classpath:fdfs.yml")
+public class FastDFSClientWrapper {
+
+    private final FastFileStorageClient storageClient;
+    private final AppendFileStorageClient appendFileStorageClient;
+    private final FdfsWebServer fdfsWebServer;
+    @Value("group1")
+    private String groupName;
+
+    /**
+     * 上传文件
+     * @param file 文件对象
+     * @return 文件访问地址
+     * @throws IOException
+     */
+    public String uploadFile(MultipartFile file) throws IOException {
+        StorePath storePath = storageClient.uploadFile(file.getInputStream(),file.getSize(), FilenameUtils.getExtension(file.getOriginalFilename()),null);
+        return getResAccessUrl(storePath);
+    }
+
+    /**
+     * 上传文件
+     * @param file 文件对象
+     * @return 文件访问地址
+     * @throws IOException
+     */
+    public String uploadFile(File file) throws IOException {
+        FileInputStream inputStream = new FileInputStream(file);
+        StorePath storePath = storageClient.uploadFile(inputStream,file.length(), FilenameUtils.getExtension(file.getName()),null);
+        return getResAccessUrl(storePath);
+    }
+
+    /**
+     * 将一段字符串生成一个文件上传
+     * @param content 文件内容
+     * @param fileExtension
+     * @return
+     */
+    public String uploadFile(String content, String fileExtension) {
+        byte[] buff = content.getBytes(Charset.forName("UTF-8"));
+        ByteArrayInputStream stream = new ByteArrayInputStream(buff);
+        StorePath storePath = storageClient.uploadFile(stream,buff.length, fileExtension,null);
+        return getResAccessUrl(storePath);
+    }
+
+    // 封装图片完整URL地址
+    private String getResAccessUrl(StorePath storePath) {
+        String fileUrl = fdfsWebServer.getWebServerUrl() + storePath.getFullPath();
+        return fileUrl;
+    }
+
+    /**
+     * 删除文件
+     * @param fileUrl 文件访问地址
+     * @return
+     */
+    public void deleteFile(String fileUrl) {
+        if (StringUtils.isEmpty(fileUrl)) {
+            return;
+        }
+        try {
+            StorePath storePath = StorePath.praseFromUrl(fileUrl);
+            storageClient.deleteFile(storePath.getGroup(), storePath.getPath());
+        } catch (FdfsUnsupportStorePathException e) {
+            log.warn(e.getMessage());
+        }
+    }
+
+    /**
+     * 下载文件文件
+     * @param fileUrl 文件访问地址断点续传
+     * @return
+     */
+    public byte[] downloadFileWithTransmission(String fileUrl) {
+        if (StringUtils.isEmpty(fileUrl)) {
+            return null;
+        }
+        try {
+            byte[] bytes = storageClient.downloadFile(groupName, fileUrl, new DownloadCallback<byte[]>() {
+                @Override
+                public byte[] recv(InputStream inputStream) throws IOException {
+                    byte[] buffer = new byte[8192];
+                    inputStream.read(buffer,0,8192);
+                    return buffer;
+                }
+            });
+            return bytes;
+        } catch (FdfsUnsupportStorePathException e) {
+            log.warn(e.getMessage());
+        }
+        return null;
+    }
+}
